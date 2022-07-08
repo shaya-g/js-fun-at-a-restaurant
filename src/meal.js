288f007767903bbdb209a8511b22647da70280e8
Shaya Golafshani <s.golaff@gmail.com>
2022-07-07 20:02:24 -0600

    Update the meal.js file

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,31 @@
-function nameMenuItem() {
-
+function nameMenuItem(item) {
+  return `Delicious ${item}`
 }
+
+function createMenuItem(name, price, type) {
+   var item = {
+   name: name,
+   price: price,
+   type: type,
+   }
+   return item;
+}
+
+ function addIngredients(food, ingredients) {
+   if (!ingredients.includes(food)) {
+     return ingredients.push(food);
+   }
+ }
+
+
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
-
