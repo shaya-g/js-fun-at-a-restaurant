9d91636bccb18156760080859a36477fc59fa135
Shaya Golafshani <s.golaff@gmail.com>
2022-07-09 11:55:56 -0600

    Update the completed meal.js file

--- src/meal.js
+++ src/meal.js
@@ -2,30 +2,45 @@ function nameMenuItem(item) {
   return `Delicious ${item}`
 }
 
-function createMenuItem(name, price, type) {
-   var item = {
-   name: name,
-   price: price,
-   type: type,
-   }
-   return item;
+function createMenuItem(itemName, itemPrice, itemType) {
+   return {
+   name: itemName,
+   price: itemPrice,
+   type: itemType,
+ };
 }
 
  function addIngredients(food, ingredients) {
    if (!ingredients.includes(food)) {
-     return ingredients.push(food);
-   }
- }
+   ingredients.push(food)
+  }
+  console.log("array:", ingredients)
+}
 
 
+function formatPrice(price) {
+  return "$" + price
+}
 
+function decreasePrice(priceOfMenuItem) {
+ var discountAmount = priceOfMenuItem * 0.1
+ return priceOfMenuItem - discountAmount
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  return {
+  title: title,
+  ingredients: ingredients,
+  type: menuItemType
+}
+}
 
 
 module.exports = {
   nameMenuItem,
   createMenuItem,
   addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
