282baaa7b776a5a0a32a285e98c3221d2a8ed4c9
Shaya Golafshani <s.golaff@gmail.com>
2022-07-09 17:31:38 -0600

    Update the order.js and order-test.js files for completion

--- src/order.js
+++ src/order.js
@@ -1,7 +1,46 @@
+function takeOrder(orderNumber, deliveryOrders) {
+  if (deliveryOrders.length < 3) {
+    deliveryOrders.push(orderNumber);
+  }
+}
+
+ function refundOrder(orderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+   if (deliveryOrders[i].orderNumber === orderNumber) {
+     deliveryOrders.splice(i, 1);
+   }
+  }
+ }
+
+function listItems(deliveryOrders) {
+ var list = [];
+ for (var i = 0; i < deliveryOrders.length; i++) {
+   list.push(deliveryOrders[i].item);
+ }
+ return list.join(`, `);
+}
+
+function searchOrder(deliveryOrders, item) {
+var result = false;
+  for (var i = 0; i < deliveryOrders.length; i++) {
+     if (deliveryOrders[i].item === item) {
+      result = true;
+    } 
+  }
+  return result;
+  }
+
+
+
+
+
+
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+   takeOrder,
+   refundOrder,
+   listItems,
+   searchOrder
 }
