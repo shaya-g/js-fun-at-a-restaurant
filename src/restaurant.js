d46a45dd897f43dc941045d1309c596727e3f119
Shaya Golafshani <s.golaff@gmail.com>
2022-07-09 21:00:00 -0600

    Update the restaurant test and file

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,35 @@
+function createRestaurant(item) {
+  var menus = {
+    breakfast: [],
+    lunch: [],
+    dinner: []
+};
+  var restaurant = {
+   name: item,
+   menus: menus
+ };
+
+  return restaurant;
+}
+
+function addMenuItem(restaurant, newItem) {
+  if (newItem.type === "lunch" && !restaurant.menus.lunch.includes(newItem)) {
+    restaurant.menus.lunch.push(newItem)
+  } else if (newItem.type === "breakfast" && !restaurant.menus.breakfast.includes(newItem)) {
+    restaurant.menus.breakfast.push(newItem)
+  } else if (newItem.type === "dinner" && !restaurant.menus.dinner.includes(newItem)) {
+    restaurant.menus.dinner.push(newItem)
+  }
+}
+
+function removeMenuItem() {
+  
+}
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+ createRestaurant,
+ addMenuItem,
+ removeMenuItem
 }
